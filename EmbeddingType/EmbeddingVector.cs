2aa45860aa2672b67ae1b917a2096afe27347625
ShivangiReja <shivangi.reja@microsoft.com>
2024-05-23 14:49:29 -0700

    Update the type

--- EmbeddingType/EmbeddingVector.cs
+++ EmbeddingType/EmbeddingVector.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Text;
 using System.Text.Json;
 
 namespace System.Numerics
@@ -11,11 +12,11 @@ namespace System.Numerics
 
         public static EmbeddingVector FromJson(ReadOnlyMemory<byte> utf8Json) => new JsonArrayVector(utf8Json);
 
-        public static EmbeddingVector FromBase64(string base64) => new Base64Vector(base64);
+        public static EmbeddingVector FromBase64(ReadOnlyMemory<byte> utf8Base64) => new Base64Vector(utf8Base64);
 
         public static EmbeddingVector<T> FromScalars<T>(ReadOnlyMemory<T> scalars) => new EmbeddingVector<T>(scalars);
 
-        public abstract void Write(Utf8JsonWriter writer, string format);
+        public abstract void Write(Stream stream, string format);
     }
 
     public sealed class EmbeddingVector<T> : EmbeddingVector
@@ -26,13 +27,14 @@ namespace System.Numerics
 
         public ReadOnlyMemory<T> Scalars => _scalars;
 
-        public override void Write(Utf8JsonWriter writer, string format)
+        public override void Write(Stream stream, string format)
         {
             if (!format.Equals("J", StringComparison.Ordinal))
             {
                 throw new NotSupportedException();
             }
 
+            using var writer = new Utf8JsonWriter(stream);
             JsonSerializer.Serialize(writer, _scalars.ToArray());
         }
     }
@@ -56,28 +58,29 @@ namespace System.Numerics
             }
         }
 
-        public override void Write(Utf8JsonWriter writer, string format)
+        public override void Write(Stream stream, string format)
         {
             if (!format.Equals("J", StringComparison.Ordinal))
             {
                 throw new NotSupportedException();
             }
 
+            using var writer = new Utf8JsonWriter(stream);
             writer.WriteRawValue(_utf8Json.Span);
         }
     }
 
     internal sealed class Base64Vector : EmbeddingVector
     {
-        private readonly string _base64;
+        private readonly ReadOnlyMemory<byte> _utf8Base64;
 
-        public Base64Vector(string base64) => _base64 = base64;
+        public Base64Vector(ReadOnlyMemory<byte> utf8Base64) => _utf8Base64 = utf8Base64;
 
         public override EmbeddingVector<T> To<T>()
         {
             try
             {
-                var decodedBytes = Convert.FromBase64String(_base64);
+                var decodedBytes = Convert.FromBase64String(Encoding.UTF8.GetString(_utf8Base64.Span));
                 var array = JsonSerializer.Deserialize<T[]>(decodedBytes);
                 return new EmbeddingVector<T>(array);
             }
@@ -87,14 +90,15 @@ namespace System.Numerics
             }
         }
 
-        public override void Write(Utf8JsonWriter writer, string format)
+        public override void Write(Stream stream, string format)
         {
             if (!format.Equals("J", StringComparison.Ordinal))
             {
                 throw new NotSupportedException();
             }
 
-            writer.WriteStringValue(_base64);
+            using var writer = new Utf8JsonWriter(stream);
+            writer.WriteStringValue(_utf8Base64.Span);
         }
     }
 }
